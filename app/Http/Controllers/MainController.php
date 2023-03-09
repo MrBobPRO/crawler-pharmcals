4fba17fb507ed21fd5c8727ebd23a688c41ba2e7
MrBobPRO <bobnuridinov@gmail.com>
2023-03-09 17:02:10 +0500

    Drug caption exploded in many columns & year added

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,15 +12,16 @@ class MainController extends Controller
 {
     public function index()
     {
-        $fromPage = 301;
-        $toPage = 400;
+        $year = 2023;
+        $fromPage = 1;
+        $toPage = 119;
 
         for ($i = $fromPage; $i <= $toPage; $i++) {
             $response = Http::withBasicAuth('Ortos', 'Ortos2023')->get('http://pharm.cals.am/pharm/report/get_data.php', [
                 'prand' => (float)rand() / (float)getrandmax(),
                 'pbtn' => 'search',
-                'pdate1' => '01-01-2018',
-                'pdate2' => '08-03-2023',
+                'pdate1' => '01-01-' . $year,
+                'pdate2' => '31-12-' . $year,
                 'pname' => '',
                 'pgeneric' => '',
                 'pdosform' => '',
@@ -28,7 +29,7 @@ class MainController extends Controller
                 'pmanuf' => '',
                 'ptype' => '1',
                 'ppage' => $i,
-                'psid' => '1822168846',
+                'psid' => '611212656',
             ]);
 
             if ($response->successful()) {
@@ -45,34 +46,52 @@ class MainController extends Controller
                 $writer->setPreCalculateFormulas(false);
 
                 for ($j = 0; $j < count($result->items); $j++) {
-                    $drug = $this->validateDrug($result->items[$j]->caption);
+                    $drug = $this->explodeeDrug($result->items[$j]->caption);
                     $count = $result->items[$j]->count;
 
                     $highestRow = $sheet->getHighestRow() + 1;
-                    $sheet->setCellValue('A' . $highestRow, $drug);
-                    $sheet->setCellValue('B' . $highestRow, $count);
+                    // add data to excel file
+                    $sheet->setCellValue('A' . $highestRow, $drug[0]); // name
+                    $sheet->setCellValue('B' . $highestRow, $drug[3]); // composition
+                    $sheet->setCellValue('C' . $highestRow, $drug[1]); // dosage
+                    $sheet->setCellValue('D' . $highestRow, $drug[2]); // address
+                    $sheet->setCellValue('E' . $highestRow, $count); // count
+                    $sheet->setCellValue('F' . $highestRow, $year); // year
                 }
 
                 // Save generated file
                 $writer->save(public_path('results.xlsx'));
             } else {
-                dd('Error while parsing page' . ($i + 1));
+                dd('Error while parsing page ' . ($i + 1) . ', year: ' . $year);
             }
         }
         return 'Success!';
     }
 
-    private function validateDrug($string): string
+    private function explodeeDrug($string): array
     {
-        // replace <br> with blanks
-        $string = str_replace('<br>', ' ', $string);
+        $exploded = explode('<br>', $string);
 
-        // strip tags
-        $string = strip_tags($string);
+        for($k = 0; $k < count($exploded); $k++) {
+            $exploded[$k] = strip_tags($exploded[$k]);
+            $exploded[$k] = Str::squish($exploded[$k]);
+        }
 
-        // remove whitespaces
-        $string = Str::squish($string);
+        // generate text for composition column from name
+        $name = $exploded[0];
+        preg_match_all('#\(((?>[^()]+)|(?R))*\)#x', $name, $composition);
+        $matches = [];
 
-        return $string;
+        // ignore unwanted items like '(+)', '(-)', '(1)'
+        foreach ($composition[0] as $item) {
+            if(strlen($item) > 3) {
+                // remove () from string
+                $matches[] = substr($item, 1, strlen($item) - 2);
+            }
+        }
+
+        $exploded[3] = implode(' + ', $matches);
+
+        return $exploded;
     }
 }
